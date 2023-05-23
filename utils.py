bd3065ea19e7b1d9754455bb7f62c30f5093ddc6
Andrey Ageew <faceofdeviler@gmail.com>
2023-05-23 21:48:14 +0300

    documentation open_file_in_zip(

--- utils.py
+++ utils.py
@@ -1,8 +1,14 @@
 from zipfile import ZipFile
+from typing import Any
 import json
 
 
-def open_file_in_zip(file_zip):
+def open_file_in_zip(file_zip: Any)->list[dict]:
+    """
+    Функция открывать zip файл, находит json и загружает из объекты
+    :param file_zip: зип файл где хранится файл с операцями
+    :return: список, состоящий из операций из файла json
+    """
     with ZipFile(file_zip, "r") as myzip:
         for item in myzip.infolist():
             if item.filename.endswith(".json"):
