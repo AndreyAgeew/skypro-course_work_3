4e0d263067ccd12d463790d45cb611f0bd97724c
Andrey Ageew <faceofdeviler@gmail.com>
2023-05-23 16:21:29 +0300

    add staticmethod: __date_reversed, __check_state, __hide_operation

--- classes/operations.py
+++ classes/operations.py
@@ -20,3 +20,18 @@ class Operations:
         operations = filter(lambda item: item.get("date"), self.all_operations)
         operations = sorted(operations, key=lambda item: item["date"])[:-6:-1]
         return operations
+
+    @staticmethod
+    def __date_reversed(date):
+        return ".".join(date.split("T")[0].split("-")[::-1])
+
+    @staticmethod
+    def __check_state(operation):
+        return "ВЫПОЛНЕНО" if operation == 'EXECUTED' else 'ОТМЕНЕННО'
+
+    @staticmethod
+    def __hide_operation(order):
+        if len(order) == 16:
+            return order[:4] + " " + order[4:6] + "**" + " **** " + order[12:16]
+        else:
+            return "**" + order[len(order) - 4:]
