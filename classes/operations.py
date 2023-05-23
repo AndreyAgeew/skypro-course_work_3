83caec1a1f4b81ef17cf3ba521df89218057e895
Andrey Ageew <faceofdeviler@gmail.com>
2023-05-23 15:56:25 +0300

    add setter and getter for all_operations

--- classes/operations.py
+++ classes/operations.py
@@ -5,3 +5,13 @@ class Operations:
 
     def __repr__(self):
         return f"Operations({self.all_operations})"
+
+    @property
+    def all_operations(self):
+        return self.__all_operations
+
+    @all_operations.setter
+    def all_operations(self, value):
+        if type(value) != list or type(value[0]) != dict:
+            raise TypeError
+        self.__all_operations = value
