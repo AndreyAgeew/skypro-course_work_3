ec7f1ee89e89e5f45f8f6b326f9967432e2718d2
Andrey Ageew <faceofdeviler@gmail.com>
2023-05-23 16:26:25 +0300

    add fixture: operations_instance

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,3 +1,5 @@
+import pytest
+
 from classes.operations import Operations
 
 
@@ -17,3 +19,65 @@ def test_operations_set_last_operations():
     assert len(operations._Operations__last_operations) == 2
     assert operations._Operations__last_operations[0]['id'] == 2
     assert operations._Operations__last_operations[1]['id'] == 1
+
+
+@pytest.fixture
+def operations_instance():
+    all_operations = [
+        {
+            'id': 1,
+            'date': '2017-01-01T12:00:00',
+            'description': 'Operation 2',
+            'from': 'Account A',
+            'to': 'Account B',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'USD'}},
+            'state': 'EXECUTED'
+
+        },
+        {
+            'id': 2,
+            'date': '2018-01-01T12:00:00',
+            'description': 'Operation 2',
+            'from': 'Account A',
+            'to': 'Account B',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'USD'}},
+            'state': 'EXECUTED'
+        },
+        {
+            'id': 3,
+            'date': '2015-01-01T12:00:00',
+            'description': 'Operation 3',
+            'from': 'Account A',
+            'to': 'Account B',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'USD'}},
+            'state': 'EXECUTED'
+        },
+        {
+            'id': 4,
+            'date': '2020-01-01T12:00:00',
+            'description': 'Operation 4',
+            'from': 'Account A',
+            'to': 'Account B',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'USD'}},
+            'state': 'EXECUTED'
+        },
+        {},
+        {
+            'id': 5,
+            'date': '2021-01-01T12:00:00',
+            'description': 'Operation 5',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'RUB'}},
+            'state': 'EXECUTED'
+        },
+        {
+            'id': 6,
+            'date': '2022-01-01T12:00:00',
+            'description': 'Operation 6',
+            'from': 'Account A',
+            'to': 'Account B',
+            'operationAmount': {'amount': 100, 'currency': {'name': 'USD'}},
+            'state': 'EXECUTED'
+        }
+
+    ]
+    return Operations(all_operations)
